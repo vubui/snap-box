d9c4bf68fa8ec32388e1e43d3a3aec7440275e66
Thach Nguyen <thachnhus@gmail.com>
2013-11-09 02:46:44 -0500

    Dropbox authen

---
+++ app/js/dropbox.js
@@ -0,0 +1,24 @@
+var client = new Dropbox.Client({key: "gzh9gtknnfo104t"});
+
+$(function () {
+	$('#loginDropbox').click(function (e) {
+		e.preventDefault();
+		// This will redirect the browser to OAuth login.
+		client.authenticate();
+	});
+
+	// Try to finish OAuth authorization.
+	client.authenticate({interactive:false}, function (error) {
+		if (error) {
+			alert('Authentication error: ' + error);
+		}
+	});
+
+	if (client.isAuthenticated()) {
+		// Client is authenticated. Display UI.
+		$('#loginDropbox').hide();
+		$('#main').show();
+	}
+
+
+});
